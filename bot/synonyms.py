1553b7d39f1025015e9e96609293748e9c7822ec
Nosirjon Karimov <karimovforyt1@gmail.com>
2026-05-18 21:31:31 +0500

    Add bot/synonyms.py

---
+++ bot/synonyms.py
@@ -0,0 +1,29 @@
+SYNONYMS = {
+    "afitant": ["afitsant", "официант", "ofitsiant", "afitsanka", "официантка"],
+    "afitsant": ["afitant", "официант", "afitsanka", "официантка"],
+    "afitsanka": ["официантка", "afitant", "afitsant", "официант"],
+    "moyka": ["мойщик", "moykachi", "avto moyka", "автомойка"],
+    "pasuda moyka": ["idish yuvuvchi", "посудомойщик", "посудомойщица"],
+    "idish yuvuvchi": ["pasuda moyka", "посудомойщик", "посудомойщица"],
+    "haydovchi": ["водитель", "driver", "shofyor"],
+    "oshpaz": ["повар", "cook", "chef", "ashpaz"],
+    "kassir": ["кассир", "cashier"],
+    "qorovul": ["охранник", "security", "soqchi"],
+    "sotuvchi": ["продавец", "seller", "savdogar"],
+    "administrator": ["администратор", "admin", "resepshinist"],
+    "tozalovchi": ["уборщица", "cleaner", "farrosh"],
+    "yukchi": ["грузчик", "loader", "hamol"],
+    "tikuvchi": ["швея", "portnixa"],
+    "nonvoy": ["пекарь", "baker"],
+    "konditer": ["кондитер", "pastry chef"],
+    "elektrik": ["электрик", "electrician"],
+    "santexnik": ["сантехник", "plumber"],
+}
+
+DISTRICTS = [
+    "yunusobod", "chilonzor", "mirzo ulugbek", "shayxontohur",
+    "olmazor", "uchtepa", "yakkasaroy", "mirobod", "bektemir",
+    "sergeli", "yashnobod", "zangiota", "toshkent", "samarqand",
+    "buxoro", "andijon", "namangan", "fargona", "nukus",
+    "qoyliq", "massa", "kuyluk", "farrux", "shahar markazi",
+]
